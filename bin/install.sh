cef2b974f95c90b607a8c6d1045ed9fde1e91f0a
Brett Roper <roper.brett@gmail.com>
2025-03-19 10:29:23 +0800

    Add config to install process

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Create the sample database
+# Set the PHP_ROOT environment variable
+export PHP_ROOT=$(git rev-parse --show-toplevel)
 
-DB_HOME=$(git rev-parse --show-toplevel)/db/
+# create a starter config file from the example file
+sed "s@{{PHP_ROOT}}@${PHP_ROOT}@" ${PHP_ROOT}/config/config-example.ini > ${PHP_ROOT}/config/config.ini
+
+# Create the sample database
+DB_HOME=${PHP_ROOT}/db/
 cd $DB_HOME
 
 sqlite3 db.sqlite3 < create.sql
--- public/admin/users.php
+++ public/admin/users.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once 'env.php';
-
 echo $db;
 
 $dsn = "sqlite:$db";
