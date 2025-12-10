dbc8ce2b85bcd16b10ccad48d6b23ec508256159
Brett Roper <roper.brett@gmail.com>
2025-12-11 07:42:26 +0800

    Rename app config

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,5 +2,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$config = parse_ini_file(__DIR__ . '/../config/config.ini', true);
+$config = parse_ini_file(__DIR__ . '/../config/app.ini', true);
 
