07af7c1e9e31dfaf1a95a842e9bf3dcaccc542bd
Brett Roper <roper.brett@gmail.com>
2025-12-10 18:12:15 +0800

    Hashed passwords

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
 insert into users
 (username, password, email, admin)
 values
-('admin', 'secret', 'admin@nowhere.com', 1),
-('scott', 'tiger', 'joe@nowhere.com', 0);
+('admin', '$2y$10$1fxpfSa8iTzhSQI8enCkBudjSFJvtoKCcLz6HAH2sG0nUGA4kCivS', 'admin@nowhere.com', 1), -- secret
+('scott', '$2y$10$.wUFeZXL8t8GQhw3NhMCouL1mVGaS5KP80Ku.xkEHSCxgjn6huFcO', 'joe@nowhere.com', 0); -- tiger
