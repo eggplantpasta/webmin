327290cb558fe38d109b1b32c83fc49142c81b09
Brett Roper <roper.brett@gmail.com>
2025-12-11 16:30:05 +0800

    MacOS compatability

--- bin/serve.sh
+++ bin/serve.sh
@@ -3,7 +3,13 @@
 # Set the PHP_ROOT as the root level of the git repo
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-xdg-open http://localhost:8080
+# MacOS compatability
+if command -v xdg-open &> /dev/null; then
+    xdg-open http://localhost:8080
+else
+    open http://localhost:8080
+fi
+
 
 # Start the server
 php -S localhost:8080 -c ${ROOT_DIR}/config/php.ini -t ${ROOT_DIR}/public
